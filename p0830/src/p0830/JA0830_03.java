becde827889c9352c2caa96dca954c9312829567
KOREAVC <KOREAVC@DESKTOP-GJSDUCF>
2023-08-30 18:12:17 +0900

    8월 30일 수업 - 변수

---
+++ p0830/src/p0830/JA0830_03.java
@@ -0,0 +1,25 @@
+package p0830;
+
+public class JA0830_03 {
+
+	public static void main(String[] args) {
+		final int NUM3 = 100; // 상수선언
+		int num1 = 550;		  // 변수선언
+		int num2 = 150;		  
+		int temp = 0;		  // 변수는 선언한 후 값이 들어가야 함. 값이 들어가기 전 사용시 에러
+		int company_intro;
+		int companyIntro;
+		System.out.println(temp);
+		System.out.println(num1+num2);
+		System.out.println(num1-num2);
+		System.out.println(num1*num2);
+		System.out.println(num1/num2);
+		System.out.println(num1%num2);
+
+		num1=10;
+		num1=300;
+
+		
+	}
+
+}
